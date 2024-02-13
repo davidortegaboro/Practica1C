f929169c644d0e72a88fbb2d5c00cc84c2b7a24f
David <davidortegaboro6667@gmail.com>
2024-02-13 19:50:41 +0100

    inicio

---
+++ src/main/java/com/DavidOrtega/Practica1C/Practica1CApplication.java
@@ -0,0 +1,13 @@
+package com.DavidOrtega.Practica1C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1CApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DavidOrtega/Practica1C/Practica1CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DavidOrtega.Practica1C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
